c9a110345208b5ef521dbeb7b5d86e9ba6e0f095
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-24 09:58:00 +0600

    applications

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@serverclusterkarmokarbi.xqmpedv.mongodb.net/?appName=ServerClusterKarmokarBijoy`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+  const jobCollctions = client.db('CareerDB').collection('jobs');
+  const applicationsCollection = client.db('CareerDB').collection('applications');
+
+  app.get('/jobs',async(req,res)=>{
+      const result = await jobCollctions.find().toArray();
+      res.send(result);
+  })
+
+  app.get("/jobs/:id",async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id:new ObjectId(id)}
+    const result = await jobCollctions.findOne(query);
+    res.send(result);
+  })
+
+  app.post('/applications',async(req,res)=>{
+     const application = req.body;
+     const result = await applicationsCollection.insertOne(application);
+     res.send(result);
+  })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('careerer code server is running.');
